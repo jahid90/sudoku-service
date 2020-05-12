cb19d588f5abef4cafc4107f3859aad27a85e826
Jahid <s.jahid.90@gmail.com>
2020-05-13 04:10:18 +0530

    add coverage for cell

--- src/test/java/org/jahiduls/sudokuservice/dao/CellTest.java
+++ src/test/java/org/jahiduls/sudokuservice/dao/CellTest.java
@@ -1,9 +1,16 @@
 package org.jahiduls.sudokuservice.dao;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.stream.IntStream;
 
 public class CellTest {
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
 
     @Test
     public void cellsCreatedFromFactoryMethodWithSameValueAreSame() {
@@ -14,7 +21,7 @@ public class CellTest {
         Cell second = Cell.of(value);
 
         Assert.assertEquals("Objects should be equal", first, second);
-        Assert.assertTrue("References should be equal", first == second);
+        Assert.assertSame("References should be equal", first, second);
     }
 
     @Test
@@ -26,6 +33,26 @@ public class CellTest {
         Cell second = Cell.builder().value(value).build();
 
         Assert.assertEquals("Objects should be equal", first, second);
-        Assert.assertFalse("References should not be equal", first == second);
+        Assert.assertNotSame("References should not be equal", first, second);
+    }
+
+    @Test
+    public void canCreateCellsViaFactoryMethod() {
+        IntStream.range(0, 10)
+                .forEach(Cell::of);
+    }
+
+    @Test
+    public void factoryMethodThrowsExceptionForInvalidLowerValue() {
+        expected.expect(RuntimeException.class);
+
+        Cell.of(-1);
+    }
+
+    @Test
+    public void factoryMethodThrowsExceptionForInvalidHigherValue() {
+        expected.expect(RuntimeException.class);
+
+        Cell.of(10);
     }
 }
