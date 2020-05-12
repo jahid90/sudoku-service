7bf4d1dddc531094ac34e0b8a4ab4536a88cc522
Jahid <s.jahid.90@gmail.com>
2020-05-12 17:18:08 +0530

    rename page controller to hello controller; fix path

--- src/main/java/org/jahiduls/sudokuservice/controller/HelloController.java
+++ src/main/java/org/jahiduls/sudokuservice/controller/HelloController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainPageController {
+public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String index() {
-        return "Hello Spring from Docker!";
+        return "Hello from SpringBoot!";
     }
 
 }
