075dd3b247d7831608ec91d01c2a79cdbfd36f90
Jahid <s.jahid.90@gmail.com>
2020-05-12 20:35:16 +0530

    add constants for basic cell values; provide factory method to map values to cells

--- src/main/java/org/jahiduls/sudokuservice/dao/Cell.java
+++ src/main/java/org/jahiduls/sudokuservice/dao/Cell.java
@@ -6,8 +6,35 @@ import lombok.Getter;
 @Builder
 public class Cell {
 
+    public static final Cell BLANK = Cell.builder().value(0).build();
+    public static final Cell ONE = Cell.builder().value(1).build();
+    public static final Cell TWO = Cell.builder().value(2).build();
+    public static final Cell THREE = Cell.builder().value(3).build();
+    public static final Cell FOUR = Cell.builder().value(4).build();
+    public static final Cell FIVE = Cell.builder().value(5).build();
+    public static final Cell SIX = Cell.builder().value(6).build();
+    public static final Cell SEVEN = Cell.builder().value(7).build();
+    public static final Cell EIGHT = Cell.builder().value(8).build();
+    public static final Cell NINE = Cell.builder().value(9).build();
+
     @Getter
     private final int value;
+
+    public static Cell of(int value) {
+        switch (value) {
+            case 0: return BLANK;
+            case 1: return ONE;
+            case 2: return TWO;
+            case 3: return THREE;
+            case 4: return FOUR;
+            case 5: return FIVE;
+            case 6: return SIX;
+            case 7: return SEVEN;
+            case 8: return EIGHT;
+            case 9: return NINE;
+            default: throw new RuntimeException("Invalid cell value. Should be in [0-9]");
+        }
+    }
 
     @Override
     public String toString() {
